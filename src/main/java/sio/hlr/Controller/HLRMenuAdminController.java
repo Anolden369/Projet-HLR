f9ad4c853c2fead8f0b86b80a3be9c0627dc8841
Maxence <archimbaud.maxence@gmail.com>
2023-10-25 13:17:36 +0200

    Ajout des id coté admin + espace Visualisation soutiens

--- src/main/java/sio/hlr/Controller/HLRMenuAdminController.java
+++ src/main/java/sio/hlr/Controller/HLRMenuAdminController.java
@@ -1,6 +1,89 @@
 package sio.hlr.Controller;
 
+import javafx.event.Event;
+import javafx.scene.control.*;
+
 public class HLRMenuAdminController {
 
 
+    @javafx.fxml.FXML
+    private Button btnCreerMatiere;
+    @javafx.fxml.FXML
+    private Button btnModifMatiere;
+    @javafx.fxml.FXML
+    private Button btnCreerSalles;
+    @javafx.fxml.FXML
+    private Button btnModifSalles;
+    @javafx.fxml.FXML
+    private Button btnVisuSoutiens;
+    @javafx.fxml.FXML
+    private Button btnStatistiquesAdmin;
+    @javafx.fxml.FXML
+    private TextField txtCreerNomMatiere;
+    @javafx.fxml.FXML
+    private Button btnValiderCreerMatiere;
+    @javafx.fxml.FXML
+    private TextField txtCreerNomSousMatiere;
+    @javafx.fxml.FXML
+    private TextField txtModifNomMatiere;
+    @javafx.fxml.FXML
+    private TextArea txtModifNomSousMatiere;
+    @javafx.fxml.FXML
+    private Button btnValiderModifMatiere;
+    @javafx.fxml.FXML
+    private ComboBox cboNomMatiere;
+    @javafx.fxml.FXML
+    private TextField txtCreerNumSalle;
+    @javafx.fxml.FXML
+    private TextField txtCreerEtage;
+    @javafx.fxml.FXML
+    private Button btnValiderCreerSalle;
+    @javafx.fxml.FXML
+    private TextField txtModfNumSalle;
+    @javafx.fxml.FXML
+    private TextField txtModifEtage;
+    @javafx.fxml.FXML
+    private Button btnValiderModifSalles;
+    @javafx.fxml.FXML
+    private TreeView tvSoutiensAdmin;
+
+    @javafx.fxml.FXML
+    public void btnCreerMatiereClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnModifMatiereClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnCreerSallesClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnModifSallesClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnVisuSoutiensClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnStatistiquesAdminClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnValiderCreerMatiere(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnValiderModifMatiereClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnValiderCreerSalleClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnValiderModifSallesClicked(Event event) {
+    }
 }
