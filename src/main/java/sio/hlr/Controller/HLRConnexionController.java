6005b3af301cfc02888b30fa83b8bee1e5d3a39b
Maxence <archimbaud.maxence@gmail.com>
2023-10-25 16:44:43 +0200

    connexion controller4

--- src/main/java/sio/hlr/Controller/HLRConnexionController.java
+++ src/main/java/sio/hlr/Controller/HLRConnexionController.java
@@ -2,19 +2,23 @@ package sio.hlr.Controller;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import sio.hlr.Tools.ConnexionBDD;
+import sio.hlr.Tools.ServicesUsers;
 
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class HLRConnexionController implements Initializable {
-
+    //FXMLLoader loader = new FXMLLoader(getClass().getResource("menu-etudiant-view.fxml"));
+    ServicesUsers servicesUsers = new ServicesUsers();
     ConnexionBDD maCnx;
     @FXML
     private TextField txtLogin;
@@ -31,18 +35,32 @@ public class HLRConnexionController implements Initializable {
 
     @FXML
     public void btnConnexionClicked(Event event) {
-        if(txtLogin.getText().equals("")){
-            Alert alert=new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erreur de saisie");
-            alert.setContentText("Veuillez votre login");
-            alert.setHeaderText("");
-            alert.showAndWait();
-        } else if(pfMdp.getText().equals("")){
-            Alert alert=new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erreur de saisie");
-            alert.setContentText("Veuillez saisir votre mot de passe");
-            alert.setHeaderText("");
-            alert.showAndWait();
+        try{
+            if(txtLogin.getText().equals("")){
+                Alert alert=new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Erreur de saisie");
+                alert.setContentText("Veuillez saisir votre login");
+                alert.setHeaderText("");
+                alert.showAndWait();
+            } else if(pfMdp.getText().equals("")){
+                Alert alert=new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Erreur de saisie");
+                alert.setContentText("Veuillez saisir votre mot de passe");
+                alert.setHeaderText("");
+                alert.showAndWait();
+            }else {
+                maCnx = new ConnexionBDD();
+                servicesUsers = new ServicesUsers();
+                //if (servicesUsers.verifLogin(txtLogin.getText(),pfMdp.getText())){
+                    //Stage newStage = new Stage();
+
+               // }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/sio/hlr/HLRApplication.java
+++ src/main/java/sio/hlr/HLRApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HLRApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HLRApplication.class.getResource("menu-admin-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HLRApplication.class.getResource("hlr-connexion-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Help Le Rebours ");
         stage.setScene(scene);
